26e5d4e03603577f335bdb70d98c03d1263865b8
singandinga86 <yamonteagudo@gmail.com>
2017-10-18 00:58:47 -0500

    merged with ffernandez22

---
+++ InfoWeb.Presentation/wwwroot/application/controllers/create-user-controller.js
@@ -0,0 +1,15 @@
+﻿var app = angular.module("InfoWeb");
+
+app.controller("CreateUserController", ["$scope", '$state', "UserListService", function ($scope, $state, UserListService) {
+    $scope.role = {};
+    $scope.acceptButtonCaption = "Crear";
+    $scope.title = "Crear Usuario";
+
+    $scope.onButtonClicked = function () {
+        RoleService.createRole($scope.role).then(function (response) {
+            $state.go("userList");
+        }, function (error) {
+
+        });
+    }
+}]);
