428835baeb626724a31fe20d08553a7bab950827
uldarico89 <vecabrera.sanz@gmail.com>
2017-11-24 01:57:28 -0500

    loading

--- InfoWeb.Presentation/wwwroot/application/controllers/login-controller.js
+++ InfoWeb.Presentation/wwwroot/application/controllers/login-controller.js
@@ -1,29 +1,51 @@
 ﻿var module = angular.module("InfoWeb");
 
-app.controller("LoginController", ['$scope', '$rootScope', 'AuthenticationService', 'NotificationService', function ($scope, $rootScope, AuthenticationService, NotificationService) {
+app.controller("LoginController", ['$scope', '$rootScope', 'AuthenticationService', 'NotificationService', '$uibModal', function ($scope, $rootScope, AuthenticationService, NotificationService, $uibModal) {
 
     $scope.userName = "";
     $scope.password = "";
     $scope.invalidCredentials = false;
     angular.element('#userName').focus();
+    var modal = null;
 
     $scope.onLoginClicked = function () {
+        modal = $uibModal.open({
+            animation: true,
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: 'application/views/login/loading-modal.html',
+            controller: "LoginController",
+            windowClass: 'clsPopupLoad',
+        });
         AuthenticationService.login($scope.userName, $scope.password).then(function (response) {
             $rootScope.$broadcast("userAuthenticated", response.data);
             $scope.invalidCredentials = false;
+            modal.close();
         }, function (error) {
             $scope.invalidCredentials = true;
+            modal.close();
         });
     }; 
 
-    $scope.sendForm = function ($event) {       
+    $scope.sendForm = function ($event) {   
+       
         if ($event.keyCode === 13) {            
             $event.preventDefault();
+            modal = $uibModal.open({
+                animation: true,
+                ariaLabelledBy: 'modal-title',
+                ariaDescribedBy: 'modal-body',
+                templateUrl: 'application/views/login/loading-modal.html',
+                controller: "LoginController",
+                windowClass: 'clsPopupLoad',
+            });
             AuthenticationService.login($scope.userName, $scope.password).then(function (response) {
                 $rootScope.$broadcast("userAuthenticated", response.data);
                 $scope.invalidCredentials = false;
+                modal.close();
             }, function (error) {
                 $scope.invalidCredentials = true;
+                modal.close();
             });
         }
 
