32e8b0b6b9cb2dde9f89ad9829c4805ad123b825
Yasmany <yamonteagudo@gmail.com>
2017-12-05 22:47:57 -0500

    Upload hours v1.0

--- InfoWeb.Presentation/wwwroot/application/controllers/upload-hours-controller.js
+++ InfoWeb.Presentation/wwwroot/application/controllers/upload-hours-controller.js
@@ -1,7 +1,9 @@
 ﻿var module = angular.module("InfoWeb");
 
-app.controller("UploadHoursController", ['$scope', '$state', 'ProjectService', 'AuthenticationService', 'UserListService', 'AssignmentService', 'ngToast',
-    function ($scope, $state, ProjectService, AuthenticationService, UserListService, AssignmentService, ngToast) {
+app.controller("UploadHoursController", ['$scope', '$state', 'ProjectService',
+                'AuthenticationService', 'UserListService', 'AssignmentService',
+                'ngToast', 'WorkedHoursService',
+                function ($scope, $state, ProjectService, AuthenticationService, UserListService, AssignmentService, ngToast, WorkedHoursService) {
 
         $scope.model = {}    
         $scope.acceptButtonCaption = "Subir";
@@ -20,7 +22,12 @@ app.controller("UploadHoursController", ['$scope', '$state', 'ProjectService', '
                 var userId = AuthenticationService.getCurrentUser().id;
                 AssignmentService.getHoursTypeByAssignment(projectSelected.id, userId).then(function (response) {
                     $scope.hourTypes = response.data;
+                    console.log(response.data);
                 }, function (error) { });
+
+               // console.log(projectSelected);
+                
+;
             }
             else
                 $scope.model.hourType = null;
@@ -31,22 +38,20 @@ app.controller("UploadHoursController", ['$scope', '$state', 'ProjectService', '
         }
 
         $scope.uploadHours = function () {
+            var userId = AuthenticationService.getCurrentUser().id;
+            WorkedHoursService.uploadHours(userId, $scope.model).then(function (response) {
+                ngToast.create({
+                    dismissButton: true,
+                    content: response.data
+                });
 
-            //AssignmentService.createProjectAssignmentGroup($scope.model).then(function (response) {
-            //    $scope.model = {}
-            //    $scope.model.usersSelected = [];
-            //    $scope.userDuplicate = false;
-            //    ngToast.create({
-            //        dismissButton: true,
-            //        content: response.data
-            //    });
-            //}, function (error) {
-            //    ngToast.create({
-            //        className: "danger",
-            //        dismissButton: true,
-            //        content: error.data.messages[0]
-            //    });
-            //});
+            }, function (error) {
+                ngToast.create({
+                    className: "danger",
+                    dismissButton: true,
+                    content: error.data.messages[0]
+                });
+            });         
         };
 
     }]);
---
+++ InfoWeb.Presentation/wwwroot/application/services/WorkedHourService.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module("InfoWeb");
+app.factory("WorkedHoursService", ['$http', "UrlService",
+    function ($http, UrlService) {
+        var baseUrl = UrlService.getApiUrlPrefix();
+        return {
+            uploadHours: function (userId, workedHoursData) {
+                return $http({
+                    method: 'POST',
+                    data: workedHoursData, 
+                    url: baseUrl+ "user/" + userId + "/WorkedHours"
+                });
+            }
+        }
+    }]);
