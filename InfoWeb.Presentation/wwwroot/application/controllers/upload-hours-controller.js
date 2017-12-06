af055d715ffb1509514378356c8db5a1fa761cb5
Yasmany <yamonteagudo@gmail.com>
2017-12-06 00:06:04 -0500

    added hours to upload hours template and fixed a bug when making a developer assignment

--- InfoWeb.Presentation/wwwroot/application/controllers/upload-hours-controller.js
+++ InfoWeb.Presentation/wwwroot/application/controllers/upload-hours-controller.js
@@ -5,53 +5,69 @@ app.controller("UploadHoursController", ['$scope', '$state', 'ProjectService',
                 'ngToast', 'WorkedHoursService',
                 function ($scope, $state, ProjectService, AuthenticationService, UserListService, AssignmentService, ngToast, WorkedHoursService) {
 
-        $scope.model = {}    
-        $scope.acceptButtonCaption = "Subir";
-        $scope.title = "Subir Horas";
+                    $scope.model = {}    
+                    $scope.acceptButtonCaption = "Subir";
+                    $scope.title = "Subir Horas";
+                    $scope.elapsedHours = 0;
 
-        ProjectService.getProjectsForUser(AuthenticationService.getCurrentUser().id).then(function (response) {
+                    ProjectService.getProjectsForUser(AuthenticationService.getCurrentUser().id).then(function (response) {
 
-            $scope.projects = response.data;
-        },
-            function (error) {
+                        $scope.projects = response.data;
+                    },
+                    function (error) {
 
-            });
-        $scope.onChangedProject = function () {
-            if ($scope.model.project) {
-                var projectSelected = $scope.model.project;
-                var userId = AuthenticationService.getCurrentUser().id;
-                AssignmentService.getHoursTypeByAssignment(projectSelected.id, userId).then(function (response) {
-                    $scope.hourTypes = response.data;
-                    console.log(response.data);
-                }, function (error) { });
+                    });
+                    $scope.onChangedProject = function () {
+                        if ($scope.model.project) {
+                            var projectSelected = $scope.model.project;
+                            var userId = AuthenticationService.getCurrentUser().id;
+                            AssignmentService.getHoursTypeByAssignment(projectSelected.id, userId).then(function (response) {
+                                $scope.hourTypes = response.data;
+                            }, function (error) { });
 
-               // console.log(projectSelected);
+                           // console.log(projectSelected);
                 
-;
-            }
-            else
-                $scope.model.hourType = null;
-        };
+            ;
+                        }
+                        else
+                            $scope.model.hourType = null;
+                    };
 
-        $scope.today = function () {
-            alert('asdasd');
-        }
+                    $scope.today = function () {
+                        alert('asdasd');
+                    }
 
-        $scope.uploadHours = function () {
-            var userId = AuthenticationService.getCurrentUser().id;
-            WorkedHoursService.uploadHours(userId, $scope.model).then(function (response) {
-                ngToast.create({
-                    dismissButton: true,
-                    content: response.data
-                });
+                    $scope.uploadHours = function () {
+                        var userId = AuthenticationService.getCurrentUser().id;
+                        WorkedHoursService.uploadHours(userId, $scope.model).then(function (response) {
+                            ngToast.create({
+                                dismissButton: true,
+                                content: response.data
+                            });
 
-            }, function (error) {
-                ngToast.create({
-                    className: "danger",
-                    dismissButton: true,
-                    content: error.data.messages[0]
-                });
-            });         
-        };
+                        }, function (error) {
+                            ngToast.create({
+                                className: "danger",
+                                dismissButton: true,
+                                content: error.data.messages[0]
+                            });
+                        });         
+                    };
+
+                    $scope.onDatesChanged = function ()
+                    {
+                        
+                        if ($scope.model.dateStart && $scope.model.dateEnd)
+                        {
+                            var start = new Date($scope.model.dateStart);
+                            var end = new Date($scope.model.dateEnd);
+
+                            var hours = Math.abs(end - start) / 36e5;
+
+                            $scope.elapsedHours = hours;
+                        }
+                    }
+
+        
 
     }]);
